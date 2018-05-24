4e96fe12f21662019f3222b83ab29f29802d771e
Harry Keartland <harrykeartland@gmail.com>
2018-05-24 20:58:23 +0100

    Initial Commit

---
+++ ImageGeneratorGUI/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ImageGeneratorGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(150, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 800);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Width";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Height";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(12, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 821);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Image Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ImageGeneratorGUI/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageGeneratorGUI
+{
+    public partial class Form1 : Form
+    {
+        public int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public static void generateImage(int width, int height, PictureBox pictureBox)
+        {
+            Bitmap img = new Bitmap(width, height);
+            Random rnd = new Random();
+            int[,] halfimg = new int[img.Width, img.Height];
+            for (int y = 0; y < img.Height / 2; y++)
+            {
+                for (int x = 0; x < img.Width / 2; x++)
+                {
+                    int num = rnd.Next(0, 2);
+                    halfimg[x, y] = num * 255;
+                }
+            }
+            for (int y = 0; y < img.Height / 2; y++)
+            {
+                for (int x = 0; x < img.Width / 2; x++)
+                {
+                    halfimg[img.Width - 1 - x, y] = halfimg[x, y];
+                }
+            }
+            for (int y = 0; y < img.Height / 2; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    halfimg[x, img.Height - 1 - y] = halfimg[x, y];
+                }
+            }
+            for (int y = 0; y < halfimg.GetLength(1); y++)
+            {
+                for (int x = 0; x < halfimg.GetLength(0); x++)
+                {
+                    img.SetPixel(x, y, Color.FromArgb(halfimg[x, y], halfimg[x, y], halfimg[x, y]));
+                }
+            }
+            Bitmap fullSize = new Bitmap(800, 800);
+            using (Graphics gr = Graphics.FromImage(fullSize))
+            {
+                gr.SmoothingMode = SmoothingMode.HighQuality;
+                gr.InterpolationMode = InterpolationMode.NearestNeighbor;
+                gr.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                gr.DrawImage(img, new Rectangle(0, 0, fullSize.Width, fullSize.Height));
+            }
+            pictureBox.Image = fullSize;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int width = 16;
+            int height = 16;
+            try
+            {
+                width = Int32.Parse(textBox1.Text);
+                height = Int32.Parse(textBox2.Text);
+            }
+            catch
+            {
+                Console.WriteLine("incorrect width and height");
+            }
+            generateImage(width, height, pictureBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                i++;
+                pictureBox1.Image.Save("file"+i+".bmp", ImageFormat.Bmp);
+            }
+            catch
+            {
+                Console.WriteLine("Unable to save");
+            }
+        }
+    }
+}
